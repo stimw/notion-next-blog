0c4f7954ad363e4cbea7cad19c76b0c023f1b045
stimw <me@stimw.com>
2023-02-03 00:26:26 +1100

    fix: change post title font

--- layouts/layout.js
+++ layouts/layout.js
@@ -81,7 +81,7 @@ const Layout = ({
       fullWidth={fullWidth}
     >
       <article>
-        <h1 className="font-bold text-3xl text-black dark:text-white">
+        <h1 className="font-title font-bold text-3xl text-black dark:text-white">
           {frontMatter.title}
         </h1>
         {frontMatter.type[0] !== 'Page' && (
