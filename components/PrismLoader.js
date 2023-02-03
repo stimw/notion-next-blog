d21d3ffb9d1045a0e9a8d34c773852f5fdb6849b
stimw <me@stimw.com>
2023-02-03 21:32:55 +1100

    fix: code block scrollbar & toolbar

--- components/PrismLoader.js
+++ components/PrismLoader.js
@@ -1,6 +1,10 @@
 import { useEffect } from 'react';
 import Prism from 'prismjs';
 import 'prismjs/plugins/autoloader/prism-autoloader';
+import 'prismjs/plugins/toolbar/prism-toolbar';
+import 'prismjs/plugins/toolbar/prism-toolbar.min.css';
+import 'prismjs/plugins/show-language/prism-show-language';
+import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard';
 import BLOG from '@/blog.config';
 
 const PrismLoader = () => {
